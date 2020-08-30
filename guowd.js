53b5d4fa1dc22c0011608845cd3aa2268d1533b1
sadik-w <1171798741@qq.com>
2020-08-30 10:59:24 +0800

    8/30

--- guowd.js
+++ guowd.js
@@ -1,30 +1,27 @@
 var input = [];
-input.push(readline());
-for (var i = 0; i < 2 * number; i++) {
+var num = parseInt(readline());
+input.push(num);
+for (var i = 0; i < 2 * num; i++) {
   input.push(readline());
 }
-getTime(input);
+time(input);
 
-function getTime(input) {
-  var number = input[0]-0;
-  var gardenLength = 0;
-  var waterNumber = 0;
-  var waterPosition = "";
-  var inputArrayOne = [];
-  var inputArrayTwo = [];
-  var time = [];
+function time(input) {
+  var number = input[0] - 0;
+  var garden = 0;
+  var water = 0;
+  var position = "";
+  var arr = [];
   for (var i = 0; i < number; i++) {
-    inputArrayOne = input[1 + 2 * i].split(" ");
-    inputArrayTwo = input[2 + 2 * i].split(" ");
-    gardenLength = parseInt(inputArrayOne[0]);
-    waterNumber = parseInt(inputArrayOne[1]);
-    waterPosition = inputArrayTwo;
-    for (var j = 0; j < waterNumber - 1; j++) {
-      time.push(Math.ceil((waterPosition[j + 1] - waterPosition[j] - 1) / 2));
+    garden = parseInt(input[2 * i + 1].split(" ")[0]);
+    water = parseInt(input[2 * i + 1].split(" ")[1]);
+    position = input[2 * i + 2].split(" ");
+    for (var j = 0; j < water - 1; j++) {
+      arr.push(Math.ceil((position[j + 1] - position[j] - 1) / 2));
     }
-    time.push(waterPosition[0] - 1);
-    time.push(gardenLength - waterPosition[waterPosition.length - 1]);
-    print(Math.max(...time) + 1);
-    time = [];
+    arr.push(position[0] - 1);
+    arr.push(garden - position[water - 1]);
+    print(Math.max(...arr) + 1);
+    arr = [];
   }
 }
--- wanghl.js
+++ wanghl.js
@@ -1,37 +1,34 @@
-var t = readline() - 0;
-var arr2 = [];
-var i = 0;
-var s = '';
-var n = 0;
-var s = 0;
-var arr = [];
-for (i = 0; i < t; i++) {
-  s = readline().split(" ");
-  n = s[0] - 0;
-  k = s[1] - 0;
-  location = [];
-  var arr = [];
-  sr = readline().split(" ");
-  for (i = 0; i < k; i++) {
-    location.push(sr[i] - 0);
-  }
-  arr.push(location[0] - 1);
-  if (k > 1) {
-    for (i = 0; i < k - 1; i++) {
-      arr.push(location[i + 1] - location[i]);
-    }
-  }
-  arr.push(n - location[k - 1]);
-  for (i = 0; i < arr.length; i++) {
-    if (i == 0 || i == arr.length - 1) {
-      arr[i]++;
-    }
-    else {
-      arr[i] = Math.ceil((arr[i] - 1) / 2) + 1;
-    }
-  }
-  arr2.push(Math.max(...arr));
+var input = [];
+var number = parseInt(readline());
+for (var i = 0; i < 2 * number; i++) {
+  input.push(readline());
 }
-arr2.forEach(e => {
-  print(e);
-})
+times(input,number);
+function times(input,number) {
+  var garden = 0;
+  var water = 0;
+  var position = '';
+  var max = 0;
+  for (var i = 0; i < number; i++) {
+    max = 0;
+    garden = input[2 * i].split(" ")[0]-0;
+    water = input[2 * i].split(" ")[1]-0;
+    position = input[2 * i + 1].split(" ");
+    for (var j = 0; j < water - 1; j++) {
+      if (Math.ceil((position[j + 1] - position[j] - 1) / 2) > max) {
+        max = Math.ceil((position[j + 1] - position[j] - 1) / 2);;
+      }
+    }
+    if(!position.includes('1')){
+      if (position[0] - 1 > max) {
+        max = position[0] - 1;
+      }
+    }
+    if(!position.includes(garden)){
+      if (garden - position[water - 1] > max) {
+        max = garden - position[water - 1];
+      }
+    }
+    print(max + 1);
+  }
+}
